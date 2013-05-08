dcb3beb76f23311a0ca7fb200113d0dcc5fe1bd2
Tyler Florez <qualiabyte@gmail.com>
2013-05-07 20:10:25 -0700

    Add argumenta-migrate script.

---
+++ bin/migrate.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# argumenta-migrate.sh
+# Apply migrations to the Argumenta database.
+
+# This script's real path.
+SCRIPT_FILE=$(readlink -f "$0")
+
+# This script's source directory.
+SOURCE_DIR=$(readlink -f `dirname "$SCRIPT_FILE"`/..)
+
+# Add node binaries to path.
+PATH="${SOURCE_DIR}/node_modules/.bin:${PATH}"
+
+# The migrations directory.
+MIGRATIONS_DIR="${SOURCE_DIR}/db/migrations"
+
+# The application mode.
+NODE_ENV=${NODE_ENV:-'development'}
+
+# The database URL for Argumenta.
+export DATABASE_URL=$(
+  node -e "
+    var coffee = require('coffee-script');
+    var url = require('./config').postgresUrl;
+    console.log(url);
+  "
+)
+
+#
+# Gets command line options.
+#
+getOpts() {
+  while [[ "$1" == -* ]]; do
+    case "$1" in
+      -d | --debug  ) DEBUG=1; shift ;;
+      -h | --help   ) usage; exit 0 ;;
+      *             ) shift ;;
+    esac
+  done
+
+  if [[ $DEBUG -eq 1 ]]; then
+    echo "Debug mode enabled."
+    set -x
+  fi
+}
+
+#
+# Main script wraps db-migrate.
+#
+main() {
+  db-migrate -m "$MIGRATIONS_DIR" "$@"
+}
+
+# Let's do this!
+main "$@"
